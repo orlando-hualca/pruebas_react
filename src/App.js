a704ce8df8a1cb4d743bf746a35fc0f4218c71da
Hualca Yavi Orlando <zeth20001@gmail.com>
2024-04-13 19:02:14 -0400

    useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default function App() {
+  const[numero,setNumero] = useState(0)
+  const incrementar = () =>{
+    setNumero(numero+1);
+  }
+  const decrementar = () =>{
+    setNumero(numero-1);
+  }
+  return(
+    <div>
+      <h1>{numero}</h1>
+      <button onClick={incrementar}>INCREMENTAR</button>
+      <button onClick={decrementar}>DECREMENTAR</button>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/components/saludo.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Saludo = (props) =>{
+
+    return(
+         <h1>{props.mensaje}</h1>
+    )
+}
